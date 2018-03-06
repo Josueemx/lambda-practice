2f9b914bb59da0b64991ed61205f498411f4d895
Josué Morales <jos_15_12_josue@hotmail.com>
2018-03-05 18:47:57 -0700

    Changes
    se le agregaron los puntos de la tarea

--- lambda_practice/Program.cs
+++ lambda_practice/Program.cs
@@ -1,6 +1,9 @@
 ﻿using lambda_practice.data;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace lambda_practice
 {
@@ -21,24 +24,49 @@ namespace lambda_practice
 
             using (var context = new DatabaseContext(options))
             {
-                //You can erase the following 3 lines if needed
-                context.Cities.ForEachAsync(c => Console.WriteLine(c.Name));
-                context.Departments.ForEachAsync(d => Console.WriteLine(d.Name));
-                context.Employees.ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
-
-                //1. List all employees whose departament has an office in Chihuahua
-
+                //NOTA: de preferencia probar de uno por uno.
+                
+                //1. List all employees whose departament has an office in Chihuahua 
+                /*var chih_deps = context.Departments.Where(d => d.Cities.Contains(context.Cities.Single(c => c.Name == "Chihuahua"))).ToList();
+                chih_deps.ForEach(d => context.Employees.ToList().ForEach(e => {
+                    if (e.DepartmentId == d.Id)
+                    {
+                        Console.WriteLine(e.FirstName + " " + e.LastName); 
+                    }
+                        
+                }));
+                Console.WriteLine("======================================================================== 1");
+                */
 
                 //2. List all departaments and the number of employees that belong to each department.
-
+                /*var groups1 = context.Employees.GroupBy(p => p.Department.Name);
+                groups1.ToList().ForEach(g => Console.WriteLine(g.Key + ": "+g.Count()));
+                Console.WriteLine("======================================================================== 2");
+                */
 
                 //3. List all remote employees. That is all employees whose living city is not the same one as their department's.
-
+                /*var employees = context.Employees.Include(e => e.City).Include(e => e.Department).Where(e => !e.Department.Cities.Contains(context.Cities.Single(c => c.Id==e.CityId))).ToList();
+                employees.ForEach(e => Console.WriteLine(e.FirstName + " " + e.LastName));
+                Console.WriteLine("======================================================================== 3");
+                */
 
                 //4. List all employees whose hire aniversary is next month.
-
+                /*int next_month = (Int32.Parse(DateTime.Now.ToString("MM"))+1);
+                next_month = next_month >=13? 1 : next_month;
+                context.Employees.ToList().ForEach(e => {
+                    if (next_month.Equals(e.HireDate.Month))
+                    {
+                        Console.WriteLine(e.FirstName + " " + e.LastName);
+                    }
+                });
+                Console.WriteLine("======================================================================== 4");
+                */
 
                 //5. List all 12 months of the year and the number of employees hired on each month.
+                /*var groups2 = context.Employees.GroupBy(p => p.HireDate.Month);
+                groups2.ToList().ForEach(g => Console.WriteLine("On " + CultureInfo.CreateSpecificCulture("en-US").DateTimeFormat.GetMonthName((int)g.Key) + ": " + g.Count() + " employees were hired"));
+                Console.WriteLine("======================================================================== 5");
+                */
             }
 
             Console.ReadLine();
